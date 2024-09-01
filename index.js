b059b4fe4e87bc408e38b59f93a2dcc860be9df7
IvanAires23 <ivanaires32@gmail.com>
2024-08-31 22:25:26 -0300

    fix: novo site

--- index.js
+++ index.js
@@ -21,8 +21,29 @@ app.post("/referencias", async (req, res) => {
     const tempDir = os.tmpdir();
     const filePath = path.join(tempDir, 'referencias.pdf');
 
-
     const terms = await generateSearchTerms(temaTcc)
+    /* const terms = [
+       'Abordagem do enfermeiro em casos de violência sexual  ',
+       'Cuidados de enfermagem para vítimas de violência sexual  ',
+       'Intervenções de enfermagem para mulheres agredidas  ',
+       'Acolhimento de mulheres vítimas de violência sexual  ',
+       'Protocolos de atendimento de enfermeiros a vítimas de violência  ',
+       'Formação de enfermeiros em violência sexual  ',
+       'Empatia no atendimento a mulheres vítimas de violência  ',
+       'Impacto da violência sexual na saúde da mulher  ',
+       'Estratégias de comunicação do enfermeiro com vítimas de violência  ',
+       'Aspectos éticos na abordagem do enfermeiro a vítimas de violência  ',
+       'Importância do suporte psicológico no atendimento de enfermagem  ',
+       'Prevenção da violência sexual: papel do enfermeiro  ',
+       'Sensibilização dos profissionais de saúde sobre violência sexual  ',
+       'Rede de apoio para mulheres vítimas de violência: papel do enfermeiro  ',
+       'Capacitação em saúde mental para enfermeiros que atendem vítimas  ',
+       'Desafios enfrentados por enfermeiros no atendimento a vítimas de violência  ',
+       'Experiência da mulher no atendimento de enfermagem após violência sexual  ',
+       'Direitos das mulheres vítimas de violência e o papel do enfermeiro  ',
+       'Atendimento multidisciplinar a mulheres vítimas de violência sexual  ',
+       'Estudo sobre a percepção das enfermeiras em relação à violência sexual  '
+     ]*/
 
     let allReferences = [];
     for (const term of terms) {
